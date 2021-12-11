8172b37a39636bb4b2b5561f88e6ba6c29a43e43
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-12-11 15:35:38 -0600

    Se creo la clase Radix y se agrego el método radix

--- src/PruebaOrdenamiento.java
+++ src/PruebaOrdenamiento.java
@@ -1,64 +1,64 @@
 class MetodosOrdenamiento{
 	
-	static long resultados[][][]=new long[4][8][4];
-	static class Burbuja {
+		static long resultados[][][]=new long[4][8][4];
+		static class Burbuja {
 		
-		public static void ordenacionBurbuja1(int nums[],int np) {
-			int numeros[]=nums.clone();
-			long comparaciones=0,intercambios=0,recorridos=0;
+			public static void ordenacionBurbuja1(int nums[],int np) {
+				int numeros[]=nums.clone();
+				long comparaciones=0,intercambios=0,recorridos=0;
 			
-			long ini = System.nanoTime();
-			recorridos+=1;
-			for (int i = 1; i < numeros.length; i++) {
+				long ini = System.nanoTime();
 				recorridos+=1;
-				for (int j = 0; j <= numeros.length-i-1; j++) {
-					comparaciones+=1;
-					if(numeros[j]>numeros[j+1]) {
-						intercambios+=1;
-						int aux = numeros[j];
-						numeros[j]=numeros[j+1];
-						numeros[j+1]=aux;
+				for (int i = 1; i < numeros.length; i++) {
+					recorridos+=1;
+					for (int j = 0; j <= numeros.length-i-1; j++) {
+						comparaciones+=1;
+						if(numeros[j]>numeros[j+1]) {
+							intercambios+=1;
+							int aux = numeros[j];
+							numeros[j]=numeros[j+1];
+							numeros[j+1]=aux;
+						}
 					}
 				}
-			}
-			long fin = System.nanoTime();
-			resultados[np][0][0]=comparaciones;
-			resultados[np][0][1]=intercambios;
-			resultados[np][0][2]=recorridos;
-			resultados[np][0][3]=fin-ini;
+				long fin = System.nanoTime();
+				resultados[np][0][0]=comparaciones;
+				resultados[np][0][1]=intercambios;
+				resultados[np][0][2]=recorridos;
+				resultados[np][0][3]=fin-ini;
 			
 		}
 		
-		public static void ordenacionBurbuja2(int nums[],int np) {
-			int numeros[]=nums.clone();
-			long comparaciones=0,intercambios=0,recorridos=0;
+			public static void ordenacionBurbuja2(int nums[],int np) {
+				int numeros[]=nums.clone();
+				long comparaciones=0,intercambios=0,recorridos=0;
 			
-			int i =1;
-			boolean ordenado=false;
-			long ini = System.nanoTime();
-			recorridos+=1;
-			while (i<numeros.length) {
-				ordenado=true;
+				int i =1;
+				boolean ordenado=false;
+				long ini = System.nanoTime();
 				recorridos+=1;
-				for (int j = 0; j < numeros.length-i; j++) {
-					comparaciones+=1;
-					if(numeros[j]>numeros[j+1]) {
-						intercambios+=1;
-						ordenado=false;
-						int aux = numeros[j];
-						numeros[j]=numeros[j+1];
-						numeros[j+1]=aux;
+				while (i<numeros.length) {
+					ordenado=true;
+					recorridos+=1;
+					for (int j = 0; j < numeros.length-i; j++) {
+						comparaciones+=1;
+						if(numeros[j]>numeros[j+1]) {
+							intercambios+=1;
+							ordenado=false;
+							int aux = numeros[j];
+							numeros[j]=numeros[j+1];
+							numeros[j+1]=aux;
+						}
 					}
+					i+=1;
 				}
-				i+=1;
-			}
-			long fin = System.nanoTime();
-			resultados[np][1][0]=comparaciones;
-			resultados[np][1][1]=intercambios;
-			resultados[np][1][2]=recorridos;
-			resultados[np][1][3]=fin-ini;
+				long fin = System.nanoTime();
+				resultados[np][1][0]=comparaciones;
+				resultados[np][1][1]=intercambios;
+				resultados[np][1][2]=recorridos;
+				resultados[np][1][3]=fin-ini;
 			
-		}
+			}
 		
 		public static void ordenacionBurbuja3(int nums[],int np) {
 			int numeros[]=nums.clone();
@@ -186,7 +186,57 @@ class MetodosOrdenamiento{
 		
 	}
 	
-	
+	static class Radix{
+		
+		public static void radix(int[]nums,int npr) {
+			int numeros[]=nums.clone();
+			long comparaciones=0,intercambios=0,recorridos=0;
+			long ini = System.nanoTime();
+			if(numeros.length == 0)
+		          return;
+		          int[][] np = new int[numeros.length][2];
+		          int[] q = new int[0x100];
+		          int i,j,k,l,f = 0;
+		          recorridos+=1;
+		          for(k=0;k<4;k++) {
+		        	  recorridos+=1;
+		             for(i=0;i<(np.length-1);i++)
+		             np[i][1] = i+1;
+		             np[i][1] = -1;
+		             recorridos+=1;
+		             for(i=0;i<q.length;i++)
+		             q[i] = -1;
+		             recorridos+=1;
+		             for(f=i=0;i<numeros.length;i++) {
+		                j = ((0xFF<<(k<<3))&numeros[i])>>(k<<3);
+		                if(q[j] == -1)
+		                l = q[j] = f;
+		             else {
+		                l = q[j];
+		                recorridos+=1;
+		                while(np[l][1] != -1)
+		                l = np[l][1];
+		                np[l][1] = f;
+		                l = np[l][1];
+		                intercambios+=1;
+		             }
+		             f = np[f][1];
+		             np[l][0] = numeros[i];
+		             np[l][1] = -1;
+		             intercambios+=1;
+		          }
+		          for(l=q[i=j=0];i<0x100;i++)
+		          for(l=q[i];l!=-1;l=np[l][1])
+		        	  numeros[j++] = np[l][0];
+		       }//for
+		          long fin = System.nanoTime();
+		          resultados[npr][5][0]=comparaciones;
+		          resultados[npr][5][1]=intercambios;
+		          resultados[npr][5][2]=recorridos;
+		          resultados[npr][5][3]=fin-ini;
+		}
+		
+	}	
 	
 	
 }
