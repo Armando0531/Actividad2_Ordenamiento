76b23e4839c02c879fe6b4c190155b1aa3bd4ba5
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-12-11 11:40:59 -0600

    Se agrega el método de ordenacionBurbuja2 a la clase Burbuja

--- src/PruebaOrdenamiento.java
+++ src/PruebaOrdenamiento.java
@@ -60,69 +60,8 @@ class MetodosOrdenamiento{
 			
 		}
 		
-		public static void ordenacionBurbuja3(int nums[],int np) {
-			int numeros[]=nums.clone();
-			long comparaciones=0,intercambios=0,recorridos=0;
-			
-			int i =1;
-			long ini = System.nanoTime();
-			recorridos+=1;
-			do{
-				boolean ordenado=true;
-				recorridos+=1;
-				for (int j = 0; j < numeros.length-i; j++) {
-					comparaciones+=1;
-					if(numeros[j]>numeros[j+1]) {
-						intercambios+=1;
-						ordenado=false;
-						int aux = numeros[j];
-						numeros[j]=numeros[j+1];
-						numeros[j+1]=aux;
-					}
-				}
-				i+=1;
-			}while (i<numeros.length);
-			long fin = System.nanoTime();
-			resultados[np][2][0]=comparaciones;
-			resultados[np][2][1]=intercambios;
-			resultados[np][2][2]=recorridos;
-			resultados[np][2][3]=fin-ini;
-			
-
-		}
-		
 	}//burbuja
 	
-	static class Insercion {
-		
-		public static void ordenacionInsercion(int nums[],int np) {
-			int numeros[]=nums.clone();
-			long comparaciones=0,intercambios=0,recorridos=0;
-			int aux;
-			
-			long ini = System.nanoTime();
-			recorridos+=1;
-			for (int i = 1; i < numeros.length; i++) {
-				aux=numeros[i];
-				comparaciones+=1;
-				recorridos+=1;
-				for (int j=i-1; j>=0 && numeros[j]>aux ; j--) {
-					comparaciones+=1;
-					numeros[j+1]=numeros[j];
-					numeros[j]=aux;
-					intercambios+=1;
-				}
-				
-			}
-			long fin = System.nanoTime();
-			resultados[np][3][0]=comparaciones;
-			resultados[np][3][1]=intercambios;
-			resultados[np][3][2]=recorridos;
-			resultados[np][3][3]=fin-ini;
-			
-		}
-		
-	}
 	
 	
 }
